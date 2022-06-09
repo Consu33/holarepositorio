b69053b852d4144aa685a515d2c31fae2ced2f75
Consu33 <consue.schuffe.t@gmail.com>
2022-06-08 20:11:45 -0400

    cambio de nombre

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Consuelo Schuffenegger Develop
+         Consuelo Schuffenegger Develop .
         </p>
         <a
           className="App-link"
